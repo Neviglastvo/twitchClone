900c01aca64d10ec7dbf5b846b11a7f307248cb9
ParadiseSeeker <paradiseseeker94@gmail.com>
2020-04-21 12:38:00 +0300

    :/

--- src/components/header/User/User.js
+++ src/components/header/User/User.js
@@ -1,9 +1,8 @@
-import React from 'react'
-import './user.sass'
-
+import React from "react"
+import "./user.sass"
 
 const User = () => {
-    return <div>I am User component</div>
+	return <div>I am User component</div>
 }
 
 export default User
